eb2b4fd5ad9164810c550b3e7fa4be9ba382eeb9
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-05-24 17:10:20 +0700

    Update AboutUs page

--- wda2024/src/App.vue
+++ wda2024/src/App.vue
@@ -4,32 +4,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 nav {
   width: 100%;
@@ -67,11 +45,6 @@ nav a:first-of-type {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 
   nav {
     text-align: left;
--- wda2024/src/views/AboutView.vue
+++ wda2024/src/views/AboutView.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    <h1>About Us</h1>
-    <p>This is the About page of our Vue.js application.</p>
+  <div class="container">
+    <div class="header">
+      <img src="../assets/signlanguage.jpg" alt="signlanguage"/>
+      <h1>Xin chào</h1>
+      <p>Về chúng tôi</p>
+    </div>
+    <div class="content">
+      <div class="section">
+        <h3>01 Arsund the world</h3>
+        <p>Background</p>
+        <p>Alipaam lorem ante, dapilen in, vivrera quis, feugiat a, tellus.
+        ru Phasellus viverra nulla ut martus varius laoreet. Quisque rut...</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AboutView'
-}
+  name: 'AboutMe',
+};
 </script>
 
-<style>
-/* Add any styles for your About page here */
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.content {
+  width: 600px;
+}
+
+.section {
+  margin-bottom: 20px;
+}
 </style>
